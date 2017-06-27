2a15977d294edc4cc603d7300e82fcc8a7c3e5f1
Marvin Reza <marvinreza@outlook.com>
2017-06-27 03:11:56 +0200

    main.cpp finished, and more initializing in the mmu's constructor. (also now prints out the result of Blarggs' test to stdout)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
+#include "cartridge.h"
+#include "gameboy.h"
+
 #include <iostream>
 #include <cstdio>
-
-#include "cartridge.h"
-
 #include <memory>
 
 int main(int argc, char* argv[])
 {
+    srand(time(nullptr));
     if(argc < 2) {
         std::cout << BOLDRED << "[ERROR]: " << RESET;
         std::cout << "Please provide a ROM file to run" << std::endl;
@@ -14,8 +15,8 @@ int main(int argc, char* argv[])
     }
 
     std::unique_ptr<Cartridge> cartridge = std::make_unique<Cartridge>(argv[1]);
-
-
+    GameBoy gameboy(cartridge);
+    gameboy.run();
 
     return 0;
 }
--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,9 +1,11 @@
 #include "mmu.h"
+#include <iostream>
 
 #include "boot_roms.h"
 
 MMU::MMU(std::unique_ptr<Cartridge>& c) : cartridge(std::move(c)) {
     this->reset();
+
     std::cout << cartridge->getFilename() << "\n";
     std::vector<u8> rom = cartridge->getData();
     switch (cartridge->getType()) {
@@ -39,24 +41,51 @@ void MMU::reset() {
     voam.resize(0xA0, 0);
     zram.resize(0x100, 0);
 
-    zram[0x05] = 0;
-    zram[0x06] = 0;
-    zram[0x07] = 0;
-    zram[0x40] = 0x91;
-    zram[0x42] = 0;
-    zram[0x43] = 0;
-    zram[0x45] = 0;
-    zram[0x47] = 0xFC;
-    zram[0x48] = 0xFF;
-    zram[0x49] = 0xFF;
-    zram[0x4A] = 0;
-    zram[0x4B] = 0;
-    zram[0x0F] = 0;
-    zram[0xFF] = 0;
+    // Memory is documented to reset to these values
+    zram[0xFF00&0xFF] = 0x00; // Reset Joystick to all unpressed
+    zram[0xFF05&0xFF] = 0x00;
+    zram[0xFF06&0xFF] = 0x00;
+    zram[0xFF07&0xFF] = 0x00;
+    zram[0xFF10&0xFF] = 0x80;
+    zram[0xFF11&0xFF] = 0xBF;
+    zram[0xFF12&0xFF] = 0xF3;
+    zram[0xFF14&0xFF] = 0xBF;
+    zram[0xFF16&0xFF] = 0x3F;
+    zram[0xFF17&0xFF] = 0x00;
+    zram[0xFF19&0xFF] = 0xBF;
+    zram[0xFF1A&0xFF] = 0x7F;
+    zram[0xFF1B&0xFF] = 0xFF;
+    zram[0xFF1C&0xFF] = 0x9F;
+    zram[0xFF1E&0xFF] = 0xBF;
+    zram[0xFF20&0xFF] = 0xFF;
+    zram[0xFF21&0xFF] = 0x00;
+    zram[0xFF22&0xFF] = 0x00;
+    zram[0xFF23&0xFF] = 0xBF;
+    zram[0xFF24&0xFF] = 0x77;
+    zram[0xFF25&0xFF] = 0xF3;
+    zram[0xFF26&0xFF] = 0xF1;
+    zram[0xFF40&0xFF] = 0x91;
+    zram[0xFF41&0xFF] = 0x85;
+    zram[0xFF42&0xFF] = 0x00;
+    zram[0xFF43&0xFF] = 0x00;
+    zram[0xFF45&0xFF] = 0x00;
+    zram[0xFF47&0xFF] = 0xFC;
+    zram[0xFF48&0xFF] = 0xFF;
+    zram[0xFF49&0xFF] = 0xFF;
+    zram[0xFF4A&0xFF] = 0x00;
+    zram[0xFF4B&0xFF] = 0x00;
+    zram[0xFFFF&0xFF] = 0x00;
 }
 
 u8 MMU::readByte(const Address& address) {
     u16 addressValue = address.getValue();
+    /*
+    if (address.isInRange(0x0, 0x7FFF)) {
+          if (address.isInRange(0x0, 0xFF) && readByte(0xFF50) != 0x1) {
+              return bootRomDMG[addressValue];
+          }
+          return mbc->readROM(address);
+    }*/
     switch(addressValue & 0xF000) {
     // ROM bank 0
     case 0x0000: case 0x1000: case 0x2000: case 0x3000:
@@ -102,6 +131,10 @@ u8 MMU::readByte(const Address& address) {
 
 void MMU::writeByte(const Address& address, u8 byte) {
     u16 addressValue = address.getValue();
+    if (address == 0xFF02 && byte == 0x81) {
+                std::cout << this->readByte(0xFF01);
+    }
+
     switch(addressValue & 0xF000) {
     // bios and ROM bank 0
     case 0x0000: case 0x1000: case 0x2000: case 0x3000:
@@ -115,12 +148,12 @@ void MMU::writeByte(const Address& address, u8 byte) {
         vram[addressValue & 0x1FFF] = byte;
         break;
 
-    // External RAM
+    // External RAM(cartridge RAM)
     case 0xA000: case 0xB000:
         mbc->writeRAM(address, byte);
         break;
 
-    // Work RAM and echo
+    // Internal work RAM and echo
     case 0xC000: case 0xD000: case 0xE000:
         wram[addressValue & 0x1FFF] = byte;
         break;
