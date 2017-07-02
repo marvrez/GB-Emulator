f27d264cbb1c52d362de64813358686719245930
Marvin Reza <marvinreza@outlook.com>
2017-07-02 18:43:21 +0200

    The class Screen now saves a pointer to the Keypad and polls key releases/presses.

--- src/gpu/screen.cpp
+++ src/gpu/screen.cpp
@@ -1,6 +1,7 @@
 #include "screen.h"
 
-Screen::Screen(std::string gametitle, u32 magnification) :
+Screen::Screen(/*std::shared_ptr<Keypad>& keypad,*/ std::string gametitle, u32 magnification) :
+    //keypad(keypad),
     magnification(magnification),
     logicalHeight(GAMEBOY_HEIGHT),
     logicalWidth(GAMEBOY_WIDTH),
@@ -19,10 +20,20 @@ void Screen::draw(const FrameBuffer &buffer, u32 scrollX, u32 scrollY, const BGP
     renderWindow.clear(sf::Color::White);
 
     sf::Event event;
-
     while(renderWindow.pollEvent(event)) {
         if(event.type == sf::Event::Closed)
             renderWindow.close();
+        else if(event.type == sf::Event::KeyPressed) {
+            KeypadKey key = SFKeyToGBKey(event.key.code);
+            if(key != KeypadKey::NONE)
+                keypad->keyDown(key);
+        }
+        else if(event.type == sf::Event::KeyReleased) {
+            KeypadKey key = SFKeyToGBKey(event.key.code);
+            if(key != KeypadKey::NONE)
+                keypad->keyUp(key);
+        }
+
     }
 
     setPixelsOnImage(buffer, scrollX, scrollY, bgPalette);
@@ -35,6 +46,10 @@ void Screen::draw(const FrameBuffer &buffer, u32 scrollX, u32 scrollY, const BGP
 
 bool Screen::isOpen() {
     return renderWindow.isOpen();
+}
+
+void Screen::setKeypad(std::shared_ptr<Keypad> keypad) {
+    this->keypad = keypad;
 }
 
 void Screen::setPixelsOnImage(const FrameBuffer& buffer, u32 scrollX, u32 scrollY, const BGPalette& bgPalette) {
@@ -53,6 +68,30 @@ void Screen::setLargePixel(u32 x, u32 y, sf::Color color) {
     for(u32 h = 0; h < pixelSize; ++h)
         for(u32 w = 0; w < pixelSize; ++w)
             this->image.setPixel((x*pixelSize) + w, (y*pixelSize) + h, color);
+}
+
+KeypadKey Screen::SFKeyToGBKey(sf::Keyboard::Key key) {
+    KeypadKey result = KeypadKey::NONE;
+    switch (key) {
+    case sf::Keyboard::Key::Return:
+        result = KeypadKey::START; break;
+    case sf::Keyboard::Key::Space:
+        result = KeypadKey::SELECT; break;
+    case sf::Keyboard::Key::Left:
+        result = KeypadKey::LEFT; break;
+    case sf::Keyboard::Key::Right:
+        result = KeypadKey::RIGHT; break;
+    case sf::Keyboard::Key::Down:
+        result = KeypadKey::DOWN; break;
+    case sf::Keyboard::Key::Up:
+        result = KeypadKey::UP; break;
+    case sf::Keyboard::Key::X:
+        result = KeypadKey::A; break;
+    case sf::Keyboard::Key::Z:
+        result = KeypadKey::B; break;
+    default: break;
+    }
+    return result;
 }
 
 sf::Color Screen::getSFMLColor(Color color) {
--- src/gpu/screen.h
+++ src/gpu/screen.h
@@ -2,20 +2,24 @@
 #define SCREEN_H
 
 #include "framebuffer.h"
-
-#include <string>
+#include "../keypad.h"
 
 #include <SFML/Graphics.hpp>
 
+#include <string>
+#include <memory>
+
 class Screen {
 public:
-    Screen(std::string gametitle, u32 magnification = 5);
+    Screen(/*std::shared_ptr<Keypad>& keypad,*/ std::string gametitle, u32 magnification = 5);
     ~Screen() = default;
 
     void draw(const FrameBuffer& buffer, u32 scrollX, u32 scrollY, const BGPalette& bgPalette) ;
     bool isOpen();
+    void setKeypad(std::shared_ptr<Keypad> keypad);
 
 private:
+    std::shared_ptr<Keypad> keypad;
     const u32 magnification;
 
     const u32 logicalWidth, logicalHeight;
@@ -30,9 +34,9 @@ private:
     void setPixelsOnImage(const FrameBuffer& buffer, u32 scrollX, u32 scrollY, const BGPalette& bgPalette);
     void setLargePixel(u32 x, u32 y, sf::Color color);
 
+    KeypadKey SFKeyToGBKey(sf::Keyboard::Key key);
+
     sf::Color getSFMLColor(Color color);
-
-
 };
 
 #endif
